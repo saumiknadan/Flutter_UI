5bdaa3e28771aa8c72084f6297f9693a1e36850c
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-05-22 02:06:45 +0600

    new file:   fitness_1/assets/font/Poppins-Bold.ttf 	new file:   fitness_1/assets/font/Poppins-Regular.ttf 	new file:   fitness_1/lib/common/color_extension.dart 	modified:   fitness_1/lib/main.dart 	new file:   fitness_1/lib/view/on_board/on_boarding_view.dart 	modified:   fitness_1/pubspec.yaml

---
+++ fitness_1/lib/common/color_extension.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Tcolor {
+  static Color get primaryColor1 => const Color(0xff92A3FD);
+  static Color get primaryColor2 => const Color(0xff9DCEFF);
+
+  static Color get secondaryColor1 => const Color(0xffC58BF2);
+  static Color get secondaryColor2 => const Color(0xffEEA4CE);
+
+  static List<Color> get PrimaryG => [primaryColor1, primaryColor2];
+  static List<Color> get SecondaryG => [secondaryColor1, secondaryColor2];
+
+  static Color get gray => const Color(0xff1D1617);
+  static Color get black => const Color(0xff76F72);
+  static Color get white => Colors.white;
+}
