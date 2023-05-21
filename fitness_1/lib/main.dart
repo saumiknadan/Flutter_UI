d12d790b185582192bd837f5c21bd3d26764eb33
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-05-22 02:28:55 +0600

    modified:   fitness_1/lib/main.dart

--- fitness_1/lib/main.dart
+++ fitness_1/lib/main.dart
@@ -15,18 +15,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fitness 3 in 1',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: Tcolor.primaryColor1,
-        fontFamily: "Poppins"
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primaryColor: Tcolor.primaryColor1,
+          fontFamily: "Poppins"),
       home: const OnBoardingView(),
     );
   }
