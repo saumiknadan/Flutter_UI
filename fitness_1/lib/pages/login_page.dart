aef3fcf63212ca6b68387679e127ca97cfc94afc
Saumik Chowdhury <47342920+saumiknadan@users.noreply.github.com>
2023-05-26 23:26:00 +0600

    day_7 complete

--- fitness_1/lib/pages/login_page.dart
+++ fitness_1/lib/pages/login_page.dart
@@ -7,8 +7,23 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // variable declare part
   String name = "";
   bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changeButton = false; // button ta k abar ager moto kore dibe
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,92 +33,131 @@ class _LoginPageState extends State<LoginPage> {
           "Login",
         ),
       ),
+      
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/img/login_image.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Text(
-              "Welcome $name",
-              style: TextStyle(
-                  fontSize: 28, color: Colors.red, fontWeight: FontWeight.bold),
-              textScaleFactor: 1,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-              child: Column(children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Enter Username", labelText: "Username"),
-                  // on change method value ta k variable er vitor set korbe
-                  onChanged: (value) {
-                    name = value;
-                    setState(() {});
-                  },
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Enter Password", labelText: "Password"),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/img/hey.png",
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Welcome $name",
+                style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold),
+                textScaleFactor: 1,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                child: Column(children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Username", labelText: "Username"),
+                    // validator
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Username can't be empty";
+                      }
+                      return null;
+                    },
 
-                InkWell(
-                  onTap: () async {
-                    setState(() {
-                      changeButton = true;
-                    });
-                    await Future.delayed(Duration(seconds: 1));
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  },
-                  child: AnimatedContainer(
-                    duration: Duration(seconds: 1),
-                    width: changeButton ? 50 : 150,
-                    height: 50,
-                    alignment: Alignment.center,
-                    child: changeButton
-                        ? Icon(
-                            Icons.done,
-                            color: Colors.white,
-                          )
-                        : Text(
-                            "Login",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18),
-                          ),
-                    decoration: BoxDecoration(
-                        color: Colors.deepPurple,
-                        borderRadius:
-                            BorderRadius.circular(changeButton ? 20 : 10)),
+                    // on change method value ta k variable er vitor set korbe
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
-                ),
+                  TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Enter Password", labelText: "Password"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password can't be empty";
+                        } else if (value!.length < 6) {
+                          return "Password length should be greated than 6";
+                        }
 
-                // ElevatedButton(
-                //   onPressed: () {
-                //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                //   },
-                //   child: Text(
-                //     "Login",
-                //     style: TextStyle(
-                //       fontSize: 20,
-                //     ),
-                //   ),
-                //   style: TextButton.styleFrom(
-                //     padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-                //   ),
-                // )
-              ]),
-            )
-          ],
+                        return null;
+                      }),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  // Elevated Button er poriborte
+                  Material(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(changeButton ? 20 : 10),
+                    child: InkWell(
+                      splashColor: Colors.red,
+                      onTap: () => moveToHome(context),
+
+                      // async {
+                      //   setState(() {
+                      //     changeButton = true;
+                      //   });
+                      //   await Future.delayed(Duration(seconds: 1));
+                      //   Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      // setState(() {
+                      //   changeButton = false;    // button ta k abar ager moto kore dibe
+                      //  });
+                      // },
+
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 50 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+
+                        //jehetu material er vitor color deya hoyeche tai ekhane decoration dile conflict korbe
+
+                        // decoration: BoxDecoration(
+                        //     color: Colors.deepPurple,
+                        //     borderRadius:
+                        //         BorderRadius.circular(changeButton ? 20 : 10)),
+                      ),
+                    ),
+                  ),
+
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  //   child: Text(
+                  //     "Login",
+                  //     style: TextStyle(
+                  //       fontSize: 20,
+                  //     ),
+                  //   ),
+                  //   style: TextButton.styleFrom(
+                  //     padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+                  //   ),
+                  // )
+                ]),
+              )
+            ],
+          ),
         ),
       ),
     );
